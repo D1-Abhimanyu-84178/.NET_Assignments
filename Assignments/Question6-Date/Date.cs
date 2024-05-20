264860996c435e7c26d64e4579b53217c93d93d2
Abhimanyu Patil <abhipatil9798@gmail.com>
2024-05-20 14:53:07 +0530

    Question6 completed

---
+++ Assignments/Question6-Date/Date.cs
@@ -0,0 +1,67 @@
+﻿global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics.Contracts;
+
+namespace Question6_Date
+{
+    public class Date
+    {
+        private int day;
+        private int year;
+        private int month;
+
+        public Date()
+        {
+
+        }
+
+        public Date(int day, int year, int month)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public void AcceptDate()
+        {
+            Console.WriteLine("Enter day: ");
+            day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter month: ");
+            month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void PrintDate()
+        {
+            Console.WriteLine($"Date: {day}/{month}/{year}");
+        }
+
+        public bool IsValid()
+        {
+            if(day <= 31 && month <=12)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "["+ "Day: " +day + "Month : " + month + "Year : "+ year +"]";
+        }
+    }
+}
+
+
---
+++ Assignments/Question6-Date/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Question6_Date
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date = new Date();
+            date.AcceptDate();
+            date.PrintDate();
+
+            bool check = date.IsValid();
+            if(check )
+            {
+                Console.WriteLine("Date is valid");
+            }
+            else
+            {
+                Console.WriteLine("Date is invalid");
+            }
+
+            Console.WriteLine(date.ToString());
+        }
+    }
+}
