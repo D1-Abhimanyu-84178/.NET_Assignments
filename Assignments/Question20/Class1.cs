f2227cc3145dfdf2e2f9a2a7ac60c8865de17f65
Abhimanyu Patil <abhipatil9798@gmail.com>
2024-05-31 17:22:57 +0530

    Question 20 Completed

---
+++ Assignments/Question20/Class1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question20
+{
+  
+    public delegate void MyDelegate();
+        public enum MyEnum
+        {
+            MARKETTING,
+            OPERATIONS,
+            TECHNOLOGY
+        }
+        [Serializable]
+        public class Person
+        {
+            public Person()
+            {
+                Birth = new Date();
+            }
+            public Person(string name, string add, Date birth)
+            {
+                this.Name = name;
+                this.Birth = birth;
+                this.Address = add;
+            }
+            private string _name;
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            private string _address;
+
+            public string Address
+            {
+                get { return _address; }
+                set { _address = value; }
+            }
+            private Date _birth;
+
+            public Date Birth
+            {
+                get { return _birth; }
+                set { _birth = value; }
+            }
+            private bool _gender;
+
+            public bool Gender
+            {
+                get { return _gender; }
+                set { _gender = value; }
+            }
+            public int Age
+            {
+
+
+                get
+                {
+                    Date tdy = new Date(DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+                    return (tdy - Birth);
+                }
+            }
+            public virtual void Accept()
+            {
+                Console.Write("Enter name:");
+                Name = Console.ReadLine();
+                Console.Write("Enter gender(M/F):");
+                Gender = Convert.ToBoolean(Console.ReadLine() == "M" ? true : false);
+                Console.Write("Enter address:");
+                Address = Console.ReadLine();
+                Birth.AcceptDate();
+            }
+            public virtual void Display()
+            {
+                Console.WriteLine(this);
+            }
+            public override string ToString()
+            {
+                return "Name: " + Name + "\nAddress: " + Address + "\nGender: " + (Gender ? "M" : "F") + "\nDOB: " + Birth + "\nAge: " + Age;
+            }
+        }
+        [Serializable]
+        public class Date
+        {
+            private int _day;
+            public Date() { }
+            public Date(int day, int month, int year)
+            {
+                Day = day;
+                Month = month;
+                Year = year;
+            }
+
+            public int Day
+            {
+                get { return _day; }
+                set { _day = value; }
+            }
+            private int _month;
+
+            public int Month
+            {
+                get { return _month; }
+                set
+                { _month = value; }
+
+            }
+            private int _year;
+
+            public int Year
+            {
+                get { return _year; }
+                set { _year = value; }
+            }
+            public override string ToString()
+            {
+                return Day + "/" + Month + "/" + Year;
+            }
+            public void AcceptDate()
+            {
+                Console.Write("Enter day for DOB:");
+                Day = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter month for DOB:");
+                Month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter year for DOB:");
+                Year = Convert.ToInt32(Console.ReadLine());
+            }
+            public void DisplayDate()
+            {
+                Console.WriteLine(this);
+            }
+            public bool isValid()
+            {
+                DateTime dateValue;
+                return DateTime.TryParse(this.ToString(), out dateValue);
+            }
+            public static int operator -(Date d1, Date d2)
+            {
+                return Math.Abs(d1.Year - d2.Year);
+            }
+        }
+        [Serializable]
+        public class Employee : Person
+        {
+            public Employee()
+            {
+                Id = Generate();
+            }
+            public Employee(MyEnum dept, string desgn, double salary, string name, Date birth, string addr) : base(name, addr, birth)
+            {
+                Dept = dept;
+                this._desgn = desgn;
+                Salary = salary;
+                Id = Generate();
+            }
+            private int _id;
+            private static int auto = 1;
+            private static int Generate()
+            {
+                return auto++;
+            }
+            public int Id
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+            private MyEnum _dept;
+
+            public MyEnum Dept
+            {
+                get { return _dept; }
+                set { _dept = value; }
+            }
+            private double _salry;
+
+            public double Salary
+            {
+                get { return _salry; }
+                set { _salry = value; }
+            }
+            private string _desgn;
+
+            public string Designation
+            {
+                get { return _desgn; }
+                set { _desgn = value; }
+            }
+            public override void Accept()
+            {
+                base.Accept();
+                Console.Write("Enter salary: ");
+                Salary = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter department 0.Marketing 2.Operations 3.Technology: ");
+                int choice = Convert.ToInt32((Console.ReadLine()));
+                switch (choice)
+                {
+                    case 0:
+                        Dept = MyEnum.MARKETTING;
+                        break;
+                    case 1:
+                        Dept = MyEnum.OPERATIONS;
+                        break;
+                    case 2:
+                        Dept = MyEnum.TECHNOLOGY;
+                        break;
+                }
+            }
+            public override void Display()
+            {
+                System.Console.WriteLine(this);
+            }
+            public override string ToString()
+            {
+                return "Id: " + Id + "\n" + base.ToString() + "\nSalary: " + Salary + "\nDesignation: " + Designation + "\n Department:" + Dept;
+            }
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj is Employee)
+                {
+                    Employee eTemp = (Employee)obj;
+                    return eTemp.Id == this.Id;
+                }
+                return false;
+            }
+        }
+        [Serializable]
+        public class Company
+        {
+            private string _name;
+            private string _address;
+            private LinkedList<Employee> _empList;
+            double _salaryExpense;
+        public event MyDelegate insert;
+            private void EventHandler()
+        {
+            Console.WriteLine("Employee inserted");
+            CalculateSalaryExpense();
+            Console.WriteLine("Total Salary Expenses: " + SalaryExpenses);
+        }
+            public Company()
+            {
+                _empList = new LinkedList<Employee>();
+            insert = EventHandler;
+            }
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            public string Address
+            {
+                get { return _address; }
+                set { _address = value; }
+            }
+
+            public double SalaryExpenses
+            {
+                get { return _salaryExpense; }
+            }
+            public LinkedList<Employee> EmpList
+            {
+                get { return _empList; }
+            }
+            public Company(string name, string address)
+            {
+                Name = name;
+                Address = address;
+                _empList = new LinkedList<Employee>();
+            }
+            public void Accept()
+            {
+                Console.Write("Enter Company Name: ");
+                Name = Console.ReadLine();
+                Console.Write("Enter Company Address: ");
+                Address = Console.ReadLine();
+            }
+            public void Display()
+            {
+                Console.WriteLine(this);
+            }
+            private void CalculateSalaryExpense()
+            {
+                _salaryExpense = 0;
+                foreach (Employee emp in EmpList)
+                {
+                    _salaryExpense += emp.Salary;
+                }
+            }
+            public void AddEmployee(Employee e)
+            {
+                EmpList.AddLast(e);
+            insert();
+            }
+            public bool RemoveEmployee(int id)
+            {
+                Employee e = new Employee();
+                e.Id = id;
+                return EmpList.Remove(e);
+            }
+            public LinkedListNode<Employee> FindEmployee(int id)
+            {
+                Employee e = new Employee();
+                e.Id = id;
+                return EmpList.Find(e);
+
+            }
+            public void PrintEmployees()
+            {
+                foreach (Employee emp in EmpList)
+                {
+                    Console.WriteLine(emp);
+                }
+            }
+            public override string ToString()
+            {
+                CalculateSalaryExpense();
+                string s1 = "\nName: " + Name + "\nCompany: " + Address + "\nSalary Expenses: " + SalaryExpenses;
+                return s1;
+            }
+        }
+
+    }
+
---
+++ Assignments/Question20/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Question20
+{
+    internal class Program
+    {
+        static int Menu()
+        {
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Add Company name and address");
+            Console.WriteLine("2. Display Company Details");
+            Console.WriteLine("3. Add Employee");
+            Console.WriteLine("4. Remove Employee");
+            Console.WriteLine("5. Find Employee");
+            Console.WriteLine("6. Print Employees");
+            Console.Write("Enter choice: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int choice;
+            int id;
+            Company company = new Company();
+
+            do
+            {
+                choice = Menu();
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Menu Exited");
+                        break;
+                    case 1:
+                        company.Accept();
+                        break;
+                    case 2:
+                        company.Display();
+                        break;
+                    case 3:
+                        Employee employee = new Employee();
+                        employee.Accept();
+                        company.AddEmployee(employee);
+                        break;
+                    case 4:
+                        Console.Write("Enter id: ");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        if (company.RemoveEmployee(id))
+                        {
+                            Console.WriteLine("Employee removed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Employee not found");
+                        };
+                        break;
+                    case 5:
+                        Console.Write("Enter id: ");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        LinkedListNode<Employee> ln = company.FindEmployee(id);
+                        if (ln == null)
+                        {
+                            Console.WriteLine("Employee not found");
+                        }
+                        else
+                        {
+                            ln.Value.Display();
+                        }
+                        break;
+                    case 6:
+                        company.PrintEmployees();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+
+                }
+            } while (choice != 0);
+        }
+    }
+}
