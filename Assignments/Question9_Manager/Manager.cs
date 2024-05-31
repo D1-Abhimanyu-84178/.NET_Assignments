a9f8e65818d137afa6ea181023d8d644350fdef0
Abhimanyu Patil <abhipatil9798@gmail.com>
2024-05-31 17:10:23 +0530

    Question9 completed

---
+++ Assignments/Question9_Manager/Manager.cs
@@ -0,0 +1,44 @@
+﻿using Question8_EmpDept;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question9_Manager
+{
+    public class Manager : Employee
+    {
+     public double Bonus
+        {
+            get; set;
+        }
+
+     public Manager(double salary, DepartmentType dept, string name, string gender, int day, int month, int year, string address, int bonus) : base(salary, "Manager", dept, name, gender, day, month, year, address)
+     { 
+            Bonus = bonus;
+            Designation = "Manager";   
+     }
+
+     public Manager()
+        {
+            Designation = "Manager";
+        }
+
+
+        public void AcceptComman()
+        {
+            base.AcceptComman();
+            Console.WriteLine("Enter the bonus: ");
+            Bonus = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Subbordinates: {Bonus}");
+        }
+    }
+
+
+}
---
+++ Assignments/Question9_Manager/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Question9_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.AcceptComman();
+            manager.Print();
+            
+        }
+    }
+}
