cd95ef0b2576ebf7acd8f444ca24dd663c4ffd0f
Abhimanyu Patil <abhipatil9798@gmail.com>
2024-05-31 17:09:07 +0530

    Question8 completed

---
+++ Assignments/Question8_EmpDept/Employee.cs
@@ -0,0 +1,81 @@
+﻿using Question6_Date;
+using Quetion7_Person;
+
+
+namespace Question8_EmpDept
+{
+
+    public enum DepartmentType
+    {
+        HR, 
+        IT, 
+        Finance, 
+        Marketing, 
+        Sales
+    }
+    public class Employee : Person
+    {
+       public  int Id {  get; set; }
+       public double Salary { get; set; }
+
+       public string Designation { get; set; }
+       public DepartmentType Dept {  get; set; }
+
+        public static int _idCounter;
+
+       //public Person Person { get; set; }
+       public Employee()
+        {
+            
+        }
+
+        // Constructor
+        public Employee(double salary, string designation, DepartmentType dept, string name, string gender,int day,int month, int year, string address) : base(name, gender, day, month, year, address)
+        {
+            Id = _idCounter;
+            Salary = salary;
+            Designation = designation;
+            Dept = dept;
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"ID : {Id}");
+            Console.WriteLine($"Salary : {Salary}");
+            Console.WriteLine($"Designation : {Designation}");
+
+
+        }
+
+        protected void AcceptComman()
+        {
+            base.Accept();
+            Console.WriteLine("Enter Id of Employee :  ");
+            Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Salary of Employee : ");
+            Salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Department of Employee : ");
+            string[] dp = Enum.GetNames<DepartmentType>();
+            Console.WriteLine("Enter Dept Type : ");
+            foreach(string department in dp)
+            {
+                Console.WriteLine($"{department}");
+            }
+           string newDept = Console.ReadLine().ToUpperInvariant();
+           Dept = (DepartmentType)Enum.Parse(typeof(DepartmentType), newDept);
+
+        }
+
+        public void acceptDesignation()
+        {
+            AcceptComman();
+            Console.WriteLine("Enter Designation of Employee : ");
+            Designation = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return $"ID: {Id}, Salary: {Salary}, Designation: {Designation}, Department: {Dept}";
+        }
+    }
+}
---
+++ Assignments/Question8_EmpDept/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Question8_EmpDept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(50000, "Software Engineer", DepartmentType.IT, "John Doe", "Male", 15, 6, 1985, "1234 Elm Street");
+            
+            Employee emp2 = new Employee();
+            emp2.Accept();
+
+            Console.WriteLine("Both the Employees are : ");
+            emp1.Print();
+            emp2.Print();
+        }
+    }
+}
